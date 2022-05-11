9da885d6f1f30e6f42454123f8f93b02e233c9ef
Cheeky97 <98615289+Cheeky97@users.noreply.github.com>
2022-05-11 15:19:42 +0100

    Products Commit

--- ecommerce/src/Components/Product.jsx
+++ ecommerce/src/Components/Product.jsx
@@ -1,12 +1,37 @@
+import { FavoriteBorderOutlined, SearchOutlined, ShoppingCartOutlined } from '@mui/icons-material'
 import React from 'react'
 import styledComponents from 'styled-components'
 
 
-const Container = styledComponents.div``
-const Product = (item) => {
+const Container = styledComponents.div`
+    flex: 1;
+    margin: 5px;
+`
+const Circle = styledComponents.div``
+const Image = styledComponents.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`
+const Info = styledComponents.div``
+const Icon = styledComponents.div``
+
+const Product = ({item}) => {
   return (
     <Container>
-        
+        <Circle/>
+        <Image src={item.img}/>
+        <Info>
+            <Icon>
+                <ShoppingCartOutlined/>
+            </Icon>
+            <Icon>
+                <SearchOutlined/>
+            </Icon>
+            <Icon>
+                <FavoriteBorderOutlined/>
+            </Icon>
+        </Info>
     </Container>
   )
 }
--- ecommerce/src/Components/Products.jsx
+++ ecommerce/src/Components/Products.jsx
@@ -3,9 +3,11 @@ import styledComponents from 'styled-components'
 import { popularProducts } from '../data'
 import Product from './Product'
 
-const Container = styledComponents.div``
+const Container = styledComponents.div`
+    display: flex;
+`
 
-const Products = (item) => {
+const Products = () => {
   return (
     <Container>
         {popularProducts.map(item => (
