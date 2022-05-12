1e8aa9c50143c3b83b29f1b13854ee4fbadde1d5
Cheeky97 <98615289+Cheeky97@users.noreply.github.com>
2022-05-12 11:25:35 +0100

    Register and Login Page

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,8 +1,11 @@
+import Cart from "./Pages/Cart";
 import Home from "./Pages/Home";
+import Login from "./Pages/Login";
 import Product from "./Pages/Product";
 import ProductList from "./Pages/ProductList";
+import Register from "./Pages/Register";
 
 const App = () => {
-  return <div> <Product/> </div>
+  return <div> <Cart/> </div>
 }
 export default App;
