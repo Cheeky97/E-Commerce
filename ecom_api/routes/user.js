7c4070beab1cffcccb026d22edfef278df229127
Cheeky97 <98615289+Cheeky97@users.noreply.github.com>
2022-05-14 14:11:17 +0100

    Update User using JWT

--- ecom_api/routes/user.js
+++ ecom_api/routes/user.js
@@ -1,5 +1,21 @@
 const router = require("express").Router();
+const User = require("../Models/User");
+const {verifyToken, verifyTokenAndAuthorization} = require("../routes/verifyToken");
 
+router.put("/:id", verifyTokenAndAuthorization, async (req,res) => {
+    if(req.body.password){
+        req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.CIPHER_PASS).toString();
+    }
 
+    try{
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        },{new: true});
+        res.status(200).json(updatedUser);
+    }catch(err){
+        res.status(500).json(err);
+    }
+
+})
 
 module.exports = router
