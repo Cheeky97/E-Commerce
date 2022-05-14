fb9a65796721784fcbcab9ccb1b8d15655a6ac2a
Cheeky97 <98615289+Cheeky97@users.noreply.github.com>
2022-05-14 16:07:33 +0100

    Full CRUD operation done

--- ecom_api/routes/user.js
+++ ecom_api/routes/user.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const User = require("../Models/User");
 const CryptoJS = require("crypto-js");
-const {verifyToken, verifyTokenAndAuthorization} = require("../routes/verifyToken");
+const {verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin} = require("../routes/verifyToken");
 
+//UPDATE USER 
 router.put("/:id", verifyTokenAndAuthorization, async (req,res) => {
     if(req.body.password){
         req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.CIPHER_PASS).toString();
@@ -17,6 +18,27 @@ router.put("/:id", verifyTokenAndAuthorization, async (req,res) => {
         res.status(500).json(err);
     }
 
+})
+
+//DELETE USER
+router.delete("/:id", verifyTokenAndAuthorization, async (req,res)=>{
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted...");
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+//GET USER
+router.get("/find/:id", verifyTokenAndAdmin, async (req,res)=>{
+    try {
+        const user = await User.findById(req.params.id);
+        const {password, ...others} = user._doc;
+        res.status(200).json(others);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 })
 
 module.exports = router
