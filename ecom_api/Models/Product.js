2b3d0fd5f93fa49bff3357c38a7c80f70d674913
Cheeky97 <98615289+Cheeky97@users.noreply.github.com>
2022-05-16 18:06:28 +0100

    Update Product.js

--- ecom_api/Models/Product.js
+++ ecom_api/Models/Product.js
@@ -7,9 +7,10 @@ const ProductSchema = mongoose.Schema(
         desc: { type:String, required: true},
         img: { type:String, required: true},
         categories: { type:Array},
-        size: { type:String },
+        size: { type:Array },
         color: { type:String },
-        price: { type:String, required: true}        
+        price: { type:String, required: true},
+        inStock: { type:Boolean, default: true }        
     },
     { timestamps: true}
 );
